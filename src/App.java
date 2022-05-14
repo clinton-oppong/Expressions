5251ab3dd630b95a9ffcc71eec89d587b7c9f303
Clinton Oppong <oppongc1@my.erau.edu>
2022-05-13 22:57:09 -0400

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int int2 = 5;
+
+        double Kilometers = (100 * 1.609344);
+        int highscore = 50;
+
+        if (highscore == 50) {
+            System.out.println("it is the esppression you are long for");
+
+        }
+
+
+        int score = 100;//# Expression
+        if (score > 99) ;
+        { // the one inbetween yep you guess it
+            System.out.println("yo u got the high score!");  // this too
+            score = 0;// even this
+        }
+
+// Statements
+
+        int myvariable = 50;
+        myvariable++;
+        myvariable--;
+        System.out.println("this shit got m twisted");
+
+        System.out.println("id dout breaken /n"
+                + "i gotta find a " +
+                "solution");
+
+    }
+
+}
